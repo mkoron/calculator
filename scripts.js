66064a431532fc946bcec4b28b908bea298aa488
Marko Koron <koron.marko@gmail.com>
2017-11-17 13:24:06 +0100

    Copied code from Codepen.io/mkoron/

---
+++ scripts.js
@@ -0,0 +1,88 @@
+$expression = "";
+$current = "";
+$calculate = false;
+$displayChars = 10;
+
+function round(value, decimals) {
+  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+
+/*Buttons*/
+$('#AC').click(function() {
+  $('#display').text("0");
+  $expression = "";
+  $current = "";
+  $calculate = false;
+});
+$('#CE').click(function() {
+  if ($current === "") {
+    $('#display').text('0')
+  } else {
+    $current = $current.slice(0, -1)
+    $('#display').text($current);
+    $expression = $expression.slice(0, -1)
+  }
+
+});
+
+$('.number').click(function() {
+  var number = $(this).text();
+
+  if ($current.length === $displayChars) {
+    return;
+  }
+  if ($expression === "") {
+    $current = number;
+    $expression = number;
+  } else {
+    $current += number;
+    $expression += number;
+  }
+  $('#display').text($current);
+  $calculate = true;
+});
+
+$('.operand').click(function() {
+  var operand = $(this).text();
+
+  if ($expression === "" && operand === "-") {
+    $expression = "-";
+    $current = "-";
+    $('#display').text($current);
+    return;
+  } else if ($calculate) {
+    $expression = round(eval($expression), 6);
+    $('#display').text($expression);
+    $expression += operand;
+    $current = "";
+    $calculate = false;
+  } else if (!$calculate) {
+    $expression = $expression.slice(0, -1) + operand;
+  }
+});
+$('#percentage').click(function() {
+  if ($calculate) {
+    $expression = $expression = round(eval($expression) / 100, 6) + "";
+    $current = $expression;
+    $('#display').text($expression);
+  }
+});
+$('#dot').click(function() {
+  if ($current === "") {
+    $current = "0.";
+    $expression = "0.";
+  } else if (!$current.match(/\./)) {
+    $current += '.';
+    $expression += '.';
+  }
+  $('#display').text($current);
+});
+$('#equal').click(function() {
+  if ($calculate) {
+    $expression = round(eval($expression), 6);
+    $('#display').text($expression);
+    $current = "";
+    $expression = "";
+    $calculate = false;
+  }
+});
