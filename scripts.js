07752bc97f7580b8200a7fa0dcd87a634f5570c9
Marko Koron <koron.marko@gmail.com>
2017-11-24 14:41:24 +0100

    Removed the global  var

--- scripts.js
+++ scripts.js
@@ -1,91 +1,82 @@
 $expression = "";
-$current = "";
 $calculate = false;
 $displayChars = 10;
 
+/*Functions*/
 function round(value, decimals) {
-  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+  return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
 }
 
 function resetAll() {
-  $current = "";
   $expression = "";
   $calculate = false;
 }
- 
+
+function calculate() {
+  if ($calculate) {
+    $("#display").text(round(eval($expression), 6));
+  }
+}
+
 /*Buttons*/
-$('#AC').click(function() {
+$("#AC").click(function() {
   resetAll();
-  $('#display').text("0");
+  $("#display").text("0");
 });
 
-$('#CE').click(function() {
-  $current = $current.slice(0, -1)
-  $expression = $expression.slice(0, -1)
-  $current.length ? $('#display').text($current) : $('#display').text('0');
+$("#CE").click(function() {
+  $current = $("#display")
+    .text()
+    .slice(0, -1);
+  $current.length ? $("#display").text($current) : $("#display").text("0");
 });
 
-$('.number').click(function() {
-  var number = $(this).text();
-
-  if ($current.length === $displayChars) {
-    return;
-  }
-  
-  if ($expression === "") {
-    $current = number;
-    $expression = number;
+$(".number").click(function() {
+  if ($("#display").text() == "0" || !$calculate) {
+    $("#display").text($(this).text());
   } else {
-    $current += number;
-    $expression += number;
+    $("#display").text($("#display").text() + $(this).text());
   }
-  $('#display').text($current);
+
   $calculate = true;
 });
 
-$('.operand').click(function() {
-  var operand = $(this).text();
-
-  if ($expression === "" && operand === "-") {
-    $expression = "-";
-    $current = "-";
-    $('#display').text($current);
+$(".operand").click(function() {
+  if ($("#display").text() == "0" && $(this).text() == "-") {
+    $("#display").text("-");
     return;
-  } else if ($calculate) {
-    $expression = round(eval($expression), 6);
-    console.log($expression);
-    $('#display').text($expression);
-    $expression += operand;
-    $current = "";
+  }
+
+  if ($calculate) {
+    $expression += $("#display").text();
+    calculate();
+    $expression += $(this).text();
     $calculate = false;
-  } else if (!$calculate) {
-    $expression = $expression.slice(0, -1) + operand;
   }
 });
 
-$('#percentage').click(function() {
+$("#percentage").click(function() {
   if ($calculate) {
-    $expression = $expression = round(eval($expression) / 100, 6) + "";
-    $current = $expression;
-    $('#display').text($expression);
+    $expression = round(eval($("#display").text()) / 100, 6) + "*";
+    $("#display").text("0");
+    $calculate = false;
   }
 });
 
-$('#dot').click(function() {
-  if ($current === "") {
-    $current = "0.";
-    $expression = "0.";
-  } else if (!$current.match(/\./)) {
-    $current += '.';
-    $expression += '.';
+$("#dot").click(function() {
+  if ($("#display").text() === "0") {
+    $("#display").text("0.");
+  } else if (
+    !$("#display")
+      .text()
+      .match(/\./)
+  ) {
+    $("#display").text($("#display").text() + ".");
   }
-  $('#display').text($current);
 });
 
-$('#equal').click(function() {
-  if ($calculate) {
-    $expression = round(eval($expression), 6);
-    $('#display').text($expression);
-    resetAll();
-  }
+$("#equal").click(function() {
+  $expression += $("#display").text();
+  calculate();
+  resetAll();
 });
